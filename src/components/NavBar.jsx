e228b22626090f5741e9f757b956bf04a4bf74c8
romain-boy <romain.boyrie@ext.adeo.com>
2024-12-08 17:06:57 +0100

    Erreur dans le titre

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,7 +13,7 @@ function NavBar(props) {
             <nav className="navbar navbar-dark fixed-top shadow p-0" style={{backgroundColor: 'black', height:'50px'}}>
                 <a className='navbar-brand col-sm-3 col-md-2 mr-0' style={{color:'white'}}>
                     <img className='d-inline-bloack align-top' alt="bank image"  src={bank} width='50'/>
-                    &nbsp; DAPP Yiel Staking (Decentralized Banking) </a>
+                    &nbsp; DAPP Yield Staking (Decentralized Banking) </a>
 
                 <ul className="navbar-nav px-3">
                      <li className="text-nowrap d-none nav-item d-sm-block">
